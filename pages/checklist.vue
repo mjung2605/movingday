0df88084e5873f27af78021e9aa6e1cafb889936
Meike Jungilligens <mjung2605@users.noreply.github.com>
2025-08-10 15:09:03 +0200

    added images + styling

--- pages/checklist.vue
+++ pages/checklist.vue
@@ -2,6 +2,15 @@
     <h1>Moving Day</h1>
     <h2>Checkliste für den Umzug</h2>
 
+    
+    <h3>Allgemeine Tipps</h3>
+    <ul>
+        <li>IKEA Raumplanung oder Grundriss der Wohnung benutzen, um Einrichtung zu planen</li>
+        <li>Check24 für z.B. Hausratversicherung, Strom- oder Internettarif benutzen</li>
+        <li>Die Online-Services der jeweiligen Stadt researchen, sonst rechtzeitig Termin machen (Ummelden Person/Auto)</li>
+        <li>App "nebenan.de" herunterladen und mit neuer Adresse anmelden, da werden oft relevante News der Nachbarschaft gepostet oder Sachen zum Verkauf/Verschenken angeboten</li>
+    </ul>
+
     <h3>Bürokratie</h3>
     <ul>
         <li>
@@ -52,7 +61,7 @@
 
     <p>Wohnbereich</p>
     <ul>
-        <li>Sofa</li>
+        <li>Sofa + Kissen/Decke</li>
         <li>Sofatisch</li>
         <li>ggf. Fernsehtisch + Fernseher</li>
         <li>Teppich</li>
@@ -77,6 +86,7 @@
         <li>Toilettenpapier, Toilettenbürste</li>
         <li>Duschmatte</li>
         <li>Stauraum: Unterschrank, falls nicht vorhanden</li>
+        <li>Kleiner Mülleimer</li>
     </ul>
 
     <p>Küche</p>
@@ -103,10 +113,11 @@
 
     <h3>Reinigung</h3>
     <ul>
-        <li>Mülleimer und Müllbeutel; Müllabfuhrkalender</li>
-        <li>Staubwedel/Swiffer (<a href="https://www.amazon.de/Swiffer-Staubmagnet-Starter-Set-Nachfülltücher-Herkömmliche/dp/B0DZ1YS3PV/">Empfehlung</a> )</li>
+        <li>Mülleimer und Müllbeutel (klein, groß, gelb, Bio)</li>
+        <li>Müllabfuhrkalender</li>
+        <li>Staubwedel/Swiffer (<a href="https://www.amazon.de/Swiffer-Staubmagnet-Starter-Set-Nachfülltücher-Herkömmliche/dp/B0DZ1YS3PV/">Empfehlung</a>)</li>
         <li>Staubsauger</li>
-        <li>Wischer/Wischmopp (<a href="https://www.amazon.de/dp/B087RT34KJ?ref=ppx_yo2ov_dt_b_fed_asin_title">Empfehlung</a> )</li>
+        <li>Wischer/Wischmopp (<a href="https://www.amazon.de/dp/B087RT34KJ?ref=ppx_yo2ov_dt_b_fed_asin_title">Empfehlung</a>)</li>
         <li>ggf. elektrische Reinigungsbürste (gut für Bad/Dusche, <a href="https://www.amazon.de/dp/B0DRG7ZPQF?ref=ppx_yo2ov_dt_b_fed_asin_title">Empfehlung</a>)</li>
         <li>Lappen/Schwämme</li>
         <li>allg. Reinigungsmittel: Universalreiniger, Badreiniger, Scheuermilch, Essigsäure/Zitronensäure, ...</li>
@@ -115,14 +126,54 @@
         <li>ggf. Handschuhe zum Reinigen</li>
     </ul>
 
+    <div class="end">
+        <p><em>Viel Erfolg und vor allem viel Spaß!</em></p>
+        <div class="sprite"></div>
+    </div>
 
-    <h3>
-        Random Tipps
-    </h3>
-    <ul>
-        <li>IKEA Raumplanung oder eigenen Grundriss benutzen, um Einrichtung zu planen</li>
-        <li>Check24 für z.B. Hausratversicherung, Strom- oder Internettarif benutzen</li>
-        <li>Die Online-Services der jeweiligen Stadt researchen, sonst rechtzeitig Termin machen (Ummelden Person/Auto)</li>
-        <li>App "nebenan.de" herunterladen und mit neuer Adresse anmelden, da werden oft relevante News der Nachbarschaft gepostet oder Sachen zum Verkauf/Verschenken angeboten</li>
-    </ul>
+
+
 </template>
+
+<style lang="css">
+    .end {
+        margin-top: 40px;
+        margin-bottom: 80px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+    h3 {
+        margin-top: 40px;
+    }
+    li {
+        padding: 4px;
+    }
+    p {
+        padding-top: 4px;
+    }
+
+    
+    .sprite {
+        flex: 0 0 auto;
+        right: 0;
+        width: 128px;
+        height: 128px;
+        background-image: url('/assets/img/cat-128.png');
+        background-size: 1280px 128px;
+        background-repeat: no-repeat;
+        animation: cat 1s steps(10) infinite;
+    }
+
+    @keyframes cat {
+        from {
+            background-position: 0 0;
+        }
+        to {
+            background-position: -1280px 0;
+        }
+    }
+
+
+
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,49 +1,134 @@
 <template>
 
-    <h1>Moving Day</h1>
-    <h2>Meikis kleine Umzugshilfe :)</h2>
+    <div class="content">
+        <h1>Moving Day</h1>
+        <h2>Meikis kleine Umzugshilfe :)</h2>
 
-    <div v-if="!authOk">
-        <p>Bitte gebe das Passwort ein (brumm brumm):</p>
-        <input type="text" v-model="inputPw" placeholder="Passwort"/>
-        <button @click="checkPw">Einloggen</button>
-        <p v-if="wrongPw">Falsches Passwort :(</p>
-    </div>
-    <div v-else-if="!user">
-        <p>Hi! Wer bist du?</p>
-        <button @click="user = 'Kathi'">Kathi</button>
-        <button @click="user = 'Jana'">Jana</button>
-    </div>
-    <div v-else>
-        <h3>Hi {{ user }}!</h3>
-        <p>Stöber gerne durch unsere übriggebliebenen Gegenstände und Möbel und like die, an denen du Interesse hast!</p>
-        <p>Für manche der gut erhaltenen bzw. teureren würde ich vielleicht etwas Geld nehmen, aber weit unter dem Marktwert bzw. sehr verhandelbar :)</p>
+        <div v-if="!authOk" class="auth">
+            <p class="pw">Bitte gebe das Passwort ein (brumm brumm):</p>
+            <input type="text" v-model="inputPw" placeholder="Passwort"/>
+            <button @click="checkPw">Einloggen</button>
+            <p v-if="wrongPw">Falsches Passwort :(</p>
+        </div>
+        <div v-else-if="!user" class="auth">
+            <p>Hi! Wer bist du?</p>
+            <button @click="user = 'Kathi'">Kathi</button>
+            <button @click="user = 'Jana'">Jana</button>
+        </div>
+        <div v-else class="auth items">
+            <h3>Hi {{ user }}!</h3>
 
-        <div v-for="i in items" :key="i.id">
-            
-            <h3>{{ i.title }}</h3>
-            <p>{{ i.desc }}</p>
-            <button 
-                v-if="!likes.includes(i.id)" 
-                @click="toggleLike(i.id, true)">
-                Interessiert
-            </button>
-            <button 
-                v-else
-                @click="toggleLike(i.id, false)">
-                Nicht Interessiert
-            </button>
+            <p>Für Tipps und alles was mir zum Umzug einfällt: <a href="/checklist">Umzugs-Checklist</a></p>
+            <p>Stöber gerne durch unsere übriggebliebenen Gegenstände und Möbel und like die, an denen du Interesse hast!</p>
+            <p>Für manche der gut erhaltenen bzw. teureren würde ich vielleicht etwas Geld nehmen, aber weit unter dem Marktwert bzw. sehr verhandelbar :)</p>
+            <p>Wenn ihr ein Bild von meiner Wand seht, dann hab ich kein Bild von dem Ding! AAAAHHHHH</p>
+
+            <div v-for="i in items" :key="i.id" class="card">
+
+                <img :src=i.image alt="Bild des Objekts" width="200px">
+                <h3>{{ i.title }}</h3>
+                <p>{{ i.desc }}</p>
+                <button 
+                    v-if="!likes.includes(i.id)" 
+                    @click="toggleLike(i.id, true)">
+                    Interessiert
+                </button>
+                <button 
+                    v-else
+                    @click="toggleLike(i.id, false)">
+                    Nicht Interessiert
+                </button>
+
+            </div>
 
         </div>
 
     </div>
 
+    
+
 </template>
+
+
+<style lang="css">
+    body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-items: center;
+        background-color: linen;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+    .content {
+        width: 300px;
+    }
+    h1 {
+        margin: 8px;
+        font-size: 40px;
+    }
+    h2 {
+        margin: 8px;
+        font-size: large;
+        font-weight: 500;
+    }
+    h3 {
+        margin: 8px;
+        font-size: medium;
+    }
+    p {
+        margin: 8px;
+
+    }
+    img {
+        margin: 8px;
+        border: solid 1px lightgrey;
+        border-radius: 4px;
+    }
+    button {
+        margin: 8px;
+        padding: 8px;
+        background-color: tan;
+        border: none;
+        border-radius: 4px;
+        color: white;
+    }
+    input {
+        border-radius: 4px;
+        margin: 4px;
+        border: solid 1px lightgrey;
+        padding: 8px;
+    }
+    .auth {
+        margin-top: 40px;
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+    }
+    .card {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background-color: white;
+        margin-top: 8px;
+        padding: 8px;
+        padding-bottom: 12px;
+        border: solid 1px lightgrey;
+        border-radius: 4px;
+    }
+    .items {
+        margin-bottom: 100px;
+    }
+
+
+
+</style>
+
+
 
 <script setup>
 
 
-    /* Simple Auth Logic */
+    /* Very Simple Auth Logic */
     const config = useRuntimeConfig();
     const pw = config.public.PASSWORT;
 
@@ -90,7 +175,7 @@
             likes.value = likes.value.filter(id => id !== itemId)
         }
 
-        // asynchrone kommunikation mit backend - json write
+        // asynchrone kommunikation mit backend - POST
         try {
             const res = await fetch('/api/likes', {
                 method: 'POST',
@@ -104,7 +189,7 @@
 
             const json = await res.json();
             console.log('json antwort:', json);
-            
+
         } catch (e) {
             console.error('Fehler', e)
         }
@@ -117,6 +202,5 @@
         await loadLikes()
     }
 })
-
 
 </script>
