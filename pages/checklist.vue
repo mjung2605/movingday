3315fd7c2ea2a6deb2d6590702f8dc56b392aea3
Meike Jungilligens <mjung2605@users.noreply.github.com>
2025-08-19 19:48:09 +0200

    styling /checklist

--- pages/checklist.vue
+++ pages/checklist.vue
@@ -1,134 +1,136 @@
 <template>
-    <h1>Moving Day</h1>
-    <h2>Checkliste für den Umzug</h2>
+    <div class="content">
+        <h1>Moving Day</h1>
+        <h2>Checkliste für den Umzug</h2>
 
-    
-    <h3>Allgemeine Tipps</h3>
-    <ul>
-        <li>IKEA Raumplanung oder Grundriss der Wohnung benutzen, um Einrichtung zu planen</li>
-        <li>Check24 für z.B. Hausratversicherung, Strom- oder Internettarif benutzen</li>
-        <li>Die Online-Services der jeweiligen Stadt researchen, sonst rechtzeitig Termin machen (Ummelden Person/Auto)</li>
-        <li>App "nebenan.de" herunterladen und mit neuer Adresse anmelden, da werden oft relevante News der Nachbarschaft gepostet oder Sachen zum Verkauf/Verschenken angeboten</li>
-    </ul>
+        
+        <h3>Allgemeine Tipps</h3>
+        <ul>
+            <li>IKEA Raumplanung oder Grundriss der Wohnung benutzen, um Einrichtung zu planen</li>
+            <li>Check24 für z.B. Hausratversicherung, Strom- oder Internettarif benutzen</li>
+            <li>Die Online-Services der jeweiligen Stadt researchen, sonst rechtzeitig Termin machen (Ummelden Person/Auto)</li>
+            <li>App "nebenan.de" herunterladen und mit neuer Adresse anmelden, da werden oft relevante News der Nachbarschaft gepostet oder Sachen zum Verkauf/Verschenken angeboten</li>
+        </ul>
 
-    <h3>Bürokratie</h3>
-    <ul>
-        <li>
-            Sich selbst ummelden auf neue Adresse - geht manchmal online, man bekommt dann Sticker für Ausweis per Post
-        </li>
-        <li>
-            Auto ummelden (geht manchmal online), Kennzeichen muss nicht unbedingt geändert werden
-        </li>
-        <li>
-            Adressen ändern bei z.B. Krankenkasse, Job, ...
-        </li>
-        <li>
-            Haftpflichtversicherung abschließen
-        </li>
-        <li>
-            Hausratversicherung abschließen
-        </li> 
-        <li>
-            Rundfunkbeitrag anmelden oder befreien (oder warten bis die sich melden)
-        </li>
-        <li>
-            Verfügbares Internet abklären (DSL, Kabel, Glasfaser), entsprechenden Internettarif abschließen
-        </li>
-        <li>
-            Eigenen Stromtarif abschließen (falls nicht Teil von Miete), denn Grundversorgung ist teuer! 
-        </li>
-    </ul>
+        <h3>Bürokratie</h3>
+        <ul>
+            <li>
+                Sich selbst ummelden auf neue Adresse - geht manchmal online, man bekommt dann Sticker für Ausweis per Post
+            </li>
+            <li>
+                Auto ummelden (geht manchmal online), Kennzeichen muss nicht unbedingt geändert werden
+            </li>
+            <li>
+                Adressen ändern bei z.B. Krankenkasse, Job, ...
+            </li>
+            <li>
+                Haftpflichtversicherung abschließen
+            </li>
+            <li>
+                Hausratversicherung abschließen
+            </li> 
+            <li>
+                Rundfunkbeitrag anmelden oder befreien (oder warten bis die sich melden)
+            </li>
+            <li>
+                Verfügbares Internet abklären (DSL, Kabel, Glasfaser), entsprechenden Internettarif abschließen
+            </li>
+            <li>
+                Eigenen Stromtarif abschließen (falls nicht Teil von Miete), denn Grundversorgung ist teuer! 
+            </li>
+        </ul>
 
-    <h3>
-        Umzugstag
-    </h3>
-    <ul>
-        <li>Kartons vorher besorgen und bepacken</li>
-        <li>Anhänger organisieren</li>
-        <li>Helfer + Verpflegung organisieren :)</li>
-        <li>ggf. Halteverbotszone einrichten</li>
-    </ul>
+        <h3>
+            Umzugstag
+        </h3>
+        <ul>
+            <li>Kartons vorher besorgen und bepacken</li>
+            <li>Anhänger organisieren</li>
+            <li>Helfer + Verpflegung organisieren :)</li>
+            <li>ggf. Halteverbotszone einrichten</li>
+        </ul>
 
-    <h3>Möbel und Einrichtung</h3>
+        <h3>Möbel und Einrichtung</h3>
 
-    <p>Eingang</p>
-    <ul>
-        <li>Fußmatte</li>
-        <li>Schuhregal</li>
-        <li>Aufhänger für Jacken</li>
-        <li>Ort für Schlüssel etc.</li>
-    </ul>
+        <p>Eingang</p>
+        <ul>
+            <li>Fußmatte</li>
+            <li>Schuhregal</li>
+            <li>Aufhänger für Jacken</li>
+            <li>Ort für Schlüssel etc.</li>
+        </ul>
 
-    <p>Wohnbereich</p>
-    <ul>
-        <li>Sofa + Kissen/Decke</li>
-        <li>Sofatisch</li>
-        <li>ggf. Fernsehtisch + Fernseher</li>
-        <li>Teppich</li>
-        <li>Stauraum: Regale, Schränke, ...</li>
-        <li>Pflanzen, Lampen, Deko</li>
-    </ul>
+        <p>Wohnbereich</p>
+        <ul>
+            <li>Sofa + Kissen/Decke</li>
+            <li>Sofatisch</li>
+            <li>ggf. Fernsehtisch + Fernseher</li>
+            <li>Teppich</li>
+            <li>Stauraum: Regale, Schränke, ...</li>
+            <li>Pflanzen, Lampen, Deko</li>
+        </ul>
 
-    <p>Schlafbereich</p>
-    <ul>
-        <li>Bett, Matratze, ggf. Matratzenschoner, Bettbezug, Bettzeug</li>
-        <li>Nachttisch + Nachttischlampe</li>
-        <li>Ladekabel</li>
-        <li>Teppich?</li>
-        <li>Stauraum: Regale, Schränke, ...</li>
-    </ul>
+        <p>Schlafbereich</p>
+        <ul>
+            <li>Bett, Matratze, ggf. Matratzenschoner, Bettbezug, Bettzeug</li>
+            <li>Nachttisch + Nachttischlampe</li>
+            <li>Ladekabel</li>
+            <li>Teppich?</li>
+            <li>Stauraum: Regale, Schränke, ...</li>
+        </ul>
 
-    <p>Bad</p>
-    <ul>
-        <li>Handtücher, Waschlappen, Bademantel</li>
-        <li>Hygieneartikel (Shampoo, Zahnputzsachen, Binden, sowas halt)</li>
-        <li>Föhn, Glätteisen/Lockenstab</li>
-        <li>Toilettenpapier, Toilettenbürste</li>
-        <li>Duschmatte</li>
-        <li>Stauraum: Unterschrank, falls nicht vorhanden</li>
-        <li>Kleiner Mülleimer</li>
-    </ul>
+        <p>Bad</p>
+        <ul>
+            <li>Handtücher, Waschlappen, Bademantel</li>
+            <li>Hygieneartikel (Shampoo, Zahnputzsachen, Binden, sowas halt)</li>
+            <li>Föhn, Glätteisen/Lockenstab</li>
+            <li>Toilettenpapier, Toilettenbürste</li>
+            <li>Duschmatte</li>
+            <li>Stauraum: Unterschrank, falls nicht vorhanden</li>
+            <li>Kleiner Mülleimer</li>
+        </ul>
 
-    <p>Küche</p>
-    <ul>
-        <li>Basic Besteck (Löffel, Messer, Gabel, ggf. Essstäbchen)</li>
-        <li>Schubladeneinsatz für Besteck etc.</li>
-        <li>Scharfe Messer (klein/groß)</li>
-        <li>Kochutensilien wie Pfannenwender, Zange, Kelle, Schneebesen, Holzlöffel...</li>
-        <li>Behälter für besagte Kochutensilien!!!</li>
-        <li>Schneidebretter</li>
-        <li>Waage</li>
-        <li>Reibe</li>
-        <li>Sparschäler</li>
-        <li>Zitronenpresse</li>
-        <li>Knoblauchpresse (eine GUTE weil sonst fuckt das mega ab)</li>
-        <li>ggf. Korkenzieher</li>
-        <li>Ofenhandschuhe</li>
-        <li>Kleingeräte wie Wasserkocher, Toaster und ggf. Kaffeemühle, Reiskocher, Mixer, ...</li>
-        <li>Tupperware</li>
-        <li>Töpfe und Pfannen</li>
-        <li>Mülleimer</li>
-        <li>Geschirr (Gläser, Tassen, Teller, Schüsseln, ...)</li>
-    </ul>
+        <p>Küche</p>
+        <ul>
+            <li>Basic Besteck (Löffel, Messer, Gabel, ggf. Essstäbchen)</li>
+            <li>Schubladeneinsatz für Besteck etc.</li>
+            <li>Scharfe Messer (klein/groß)</li>
+            <li>Kochutensilien wie Pfannenwender, Zange, Kelle, Schneebesen, Holzlöffel...</li>
+            <li>Behälter für besagte Kochutensilien!!!</li>
+            <li>Schneidebretter</li>
+            <li>Waage</li>
+            <li>Reibe</li>
+            <li>Sparschäler</li>
+            <li>Zitronenpresse</li>
+            <li>Knoblauchpresse (eine GUTE weil sonst fuckt das mega ab)</li>
+            <li>ggf. Korkenzieher</li>
+            <li>Ofenhandschuhe</li>
+            <li>Kleingeräte wie Wasserkocher, Toaster und ggf. Kaffeemühle, Reiskocher, Mixer, ...</li>
+            <li>Tupperware</li>
+            <li>Töpfe und Pfannen</li>
+            <li>Mülleimer</li>
+            <li>Geschirr (Gläser, Tassen, Teller, Schüsseln, ...)</li>
+        </ul>
 
-    <h3>Reinigung</h3>
-    <ul>
-        <li>Mülleimer und Müllbeutel (klein, groß, gelb, Bio)</li>
-        <li>Müllabfuhrkalender</li>
-        <li>Staubwedel/Swiffer (<a href="https://www.amazon.de/Swiffer-Staubmagnet-Starter-Set-Nachfülltücher-Herkömmliche/dp/B0DZ1YS3PV/">Empfehlung</a>)</li>
-        <li>Staubsauger</li>
-        <li>Wischer/Wischmopp (<a href="https://www.amazon.de/dp/B087RT34KJ?ref=ppx_yo2ov_dt_b_fed_asin_title">Empfehlung</a>)</li>
-        <li>ggf. elektrische Reinigungsbürste (gut für Bad/Dusche, <a href="https://www.amazon.de/dp/B0DRG7ZPQF?ref=ppx_yo2ov_dt_b_fed_asin_title">Empfehlung</a>)</li>
-        <li>Lappen/Schwämme</li>
-        <li>allg. Reinigungsmittel: Universalreiniger, Badreiniger, Scheuermilch, Essigsäure/Zitronensäure, ...</li>
-        <li>Küchenreinigung: Spülmittel, Lappen/Schwamm, Trockentücher, Stahlwolle, Metallschaber</li>
-        <li>Textil-/Kleidungsreinigung: Waschmittel, Fleckenspray/Gallseife, ggf. Schuhcreme etc.</li>
-        <li>ggf. Handschuhe zum Reinigen</li>
-    </ul>
+        <h3>Reinigung</h3>
+        <ul>
+            <li>Mülleimer und Müllbeutel (klein, groß, gelb, Bio)</li>
+            <li>Müllabfuhrkalender</li>
+            <li>Staubwedel/Swiffer (<a href="https://www.amazon.de/Swiffer-Staubmagnet-Starter-Set-Nachfülltücher-Herkömmliche/dp/B0DZ1YS3PV/">Empfehlung</a>)</li>
+            <li>Staubsauger</li>
+            <li>Wischer/Wischmopp (<a href="https://www.amazon.de/dp/B087RT34KJ?ref=ppx_yo2ov_dt_b_fed_asin_title">Empfehlung</a>)</li>
+            <li>ggf. elektrische Reinigungsbürste (gut für Bad/Dusche, <a href="https://www.amazon.de/dp/B0DRG7ZPQF?ref=ppx_yo2ov_dt_b_fed_asin_title">Empfehlung</a>)</li>
+            <li>Lappen/Schwämme</li>
+            <li>allg. Reinigungsmittel: Universalreiniger, Badreiniger, Scheuermilch, Essigsäure/Zitronensäure, ...</li>
+            <li>Küchenreinigung: Spülmittel, Lappen/Schwamm, Trockentücher, Stahlwolle, Metallschaber</li>
+            <li>Textil-/Kleidungsreinigung: Waschmittel, Fleckenspray/Gallseife, ggf. Schuhcreme etc.</li>
+            <li>ggf. Handschuhe zum Reinigen</li>
+        </ul>
 
-    <div class="end">
-        <p><em>Viel Erfolg und vor allem viel Spaß!</em></p>
-        <div class="sprite"></div>
+        <div class="end">
+            <p><em>Viel Erfolg und vor allem viel Spaß!</em></p>
+            <div class="sprite"></div>
+        </div>
     </div>
 
 
@@ -136,6 +138,18 @@
 </template>
 
 <style lang="css">
+
+    body {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-items: center;
+        background-color: linen;
+        font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+    }
+    .content {
+        width: 300px;
+    }
     .end {
         margin-top: 40px;
         margin-bottom: 80px;
@@ -143,16 +157,28 @@
         flex-direction: column;
         align-items: center;
     }
+    h1 {
+        margin: 8px;
+        font-size: 40px;
+    }
+    h2 {
+        margin: 8px;
+        font-size: large;
+        font-weight: 500;
+    }
     h3 {
         margin-top: 40px;
     }
     li {
-        padding: 4px;
+        padding-top: 4px;
+        padding-bottom: 4px;
     }
     p {
         padding-top: 4px;
     }
-
+    a {
+        color: rgb(170, 132, 82);
+    }
     
     .sprite {
         flex: 0 0 auto;
